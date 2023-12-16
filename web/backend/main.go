fb4c6d1b4b946ea2375fa8e376ed44f6fefaa339
0xfedcafe <17340030+0xfedcafe@users.noreply.github.com>
2023-12-16 22:44:06 +0100

    backend: transactions implemented, public key works

--- web/backend/main.go
+++ web/backend/main.go
@@ -8,6 +8,7 @@ func main() {
 	r := gin.Default()
 	r.POST("/authorize_handler", authorizeHandler)
 	err := r.Run()
+
 	if err != nil {
 		return
 	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
