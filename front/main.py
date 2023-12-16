45148787b5b74f82f2c35804895989b404dfdba0
ge48huc <ge48huc@mytum.de>
2023-12-16 18:36:54 +0100

    Test Poetry

--- front/main.py
+++ front/main.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import poetry as pt
 
 def main():
+    bg_color = "#000000"  # Change this to your desired color code
+    css = f"""
+        <style>
+            body {{
+                background-color: {bg_color};
+            }}
+        </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
     st.title("My Streamlit App")
+    st.write("This is a Streamlit app with a custom background color.")
+
 
 if __name__ == "__main__":
     main()
