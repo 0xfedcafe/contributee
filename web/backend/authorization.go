b2ba6d33abf47d762d9a0486adf5b89a03716377
0xfedcafe <17340030+0xfedcafe@users.noreply.github.com>
2023-12-16 18:47:27 +0100

    backend: go init

---
+++ web/backend/authorization.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func authorize_handler(c gin.Context) {
+	_ := c.PostFormArray("card_number")
+	url := "https://api.ammer.io/wallet-proxy/metadata/metadata/cardId"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("accept", "application/json")
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	fmt.Println(string(body))
+}
