aa3665d19cac34fb2bff88283e1ddd6cdb2f994c
0xfedcafe <17340030+0xfedcafe@users.noreply.github.com>
2023-12-16 19:04:34 +0100

    backend: not working handlers

--- web/backend/authorization.go
+++ web/backend/authorization.go
@@ -7,13 +7,27 @@ import (
 	"net/http"
 )
 
-func authorize_handler(c gin.Context) {
-	_ = c.PostFormArray("card_number")
+func authorizeHandler(c *gin.Context) {
+	cardNumber := c.PostForm("card_number")
+	fmt.Println(cardNumber)
+
+	url := "https://api.ammer.io/wallet-proxy/api/cardUuidByNumber/cardNumber"
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s%s", url, cardNumber), nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+
+	getPublicKey(body)
+}
+
+func getPublicKey(UUID string) {
 	url := "https://api.ammer.io/wallet-proxy/metadata/metadata/cardId"
-	req, _ := http.NewRequest("GET", url, nil)
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/%s", url, UUID), nil)
 	req.Header.Add("accept", "application/json")
 	res, _ := http.DefaultClient.Do(req)
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
-	fmt.Println(string(body))
+	fmt.Println(body)
 }
--- web/backend/main.go
+++ web/backend/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.POST("", authorizeHandler)
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
