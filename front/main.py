3c847f680994ea38a382aa2d7169992160eecf5e
Your Name <you@example.com>
2023-12-16 19:47:29 +0100

    Add login

--- front/main.py
+++ front/main.py
@@ -1,18 +1,37 @@
 import streamlit as st
+import hmac
+
 
 def main():
-    bg_color = "#000000"  # Change this to your desired color code
-    css = f"""
-        <style>
-            body {{
-                background-color: {bg_color};
-            }}
-        </style>
-    """
-    st.markdown(css, unsafe_allow_html=True)
+    login()
 
-    st.title("My Streamlit App")
-    st.write("This is a Streamlit app with a custom background color.")
+def login():
+    def check_password():
+        def password_entered():
+            if hmac.compare_digest(st.session_state["password"], "a"):
+                st.session_state["password_correct"] = True
+                del st.session_state["password"]  # Don't store the password.
+            else:
+                st.session_state["password_correct"] = False
+
+        # Return True if the password is validated.
+        if st.session_state.get("password_correct", False):
+            return True
+
+        # Show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        if "password_correct" in st.session_state:
+            st.error("😕 Password incorrect")
+        return False
+
+    if not check_password():
+        st.stop()  # Do not continue if check_password is not True.
+
+    # Main Streamlit app starts here
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
 
 
 if __name__ == "__main__":
