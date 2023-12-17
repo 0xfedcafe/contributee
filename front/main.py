2a9ba785404097c21cae6f4601deaad3456efe26
ge48huc <ge48huc@mytum.de>
2023-12-17 02:16:53 +0100

    Add funding skeleton and exchange file

--- front/main.py
+++ front/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 url = "https://backend.com"
-st.set_page_config(layout = "wide")
+st.set_page_config(layout="wide")
 
 st.markdown("""
 <style>
@@ -18,16 +18,18 @@ centered_welcome_title_html = """
     </div>
 """
 
+
 def make_post_request(data):
     response = requests.post(url, data=data)
     print(data)
     return response
 
+
 def login_page():
     st.markdown(centered_welcome_title_html, unsafe_allow_html=True)
     user_input = st.text_input("Enter your card number here", type="password", key="password")
 
-    col1, col2, col3 = st.columns([0.45,0.14,0.45])
+    col1, col2, col3 = st.columns([0.45, 0.14, 0.45])
 
     with col2:
         login_button = st.button("Log in", use_container_width=True)
@@ -35,8 +37,10 @@ def login_page():
     if login_button:
         make_post_request(user_input)
 
+
 def main():
     login_page()
+
 
 if __name__ == "__main__":
     main()
