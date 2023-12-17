783bbd81f6b0b966b5c7e18f869113ff87664e1f
Your Name <you@example.com>
2023-12-17 01:30:19 +0100

    Branches wrecked

--- front/pages/donate.py
+++ front/pages/donate.py
@@ -1,24 +1,20 @@
 import streamlit as st
 
-centered_title_html = """
-    <div style="display: flex; justify-content: center; align-items: center;">
-        <h1 style="text-align: center;">Make a donation!</h1>
-    </div>
-"""
+st.set_page_config(layout="wide")
 
 centered_subtitle_html = """
-    <div style="display: flex; justify-content: center; align-items: center;">
-        <h1 style="text-align: center; font-size: 20px;">Donate to transform lives in rural villages!</h1>
+    <div style="white-space: nowrap; display: inline; justify-content: center; align-items: center;">
+        <h1 style="text-align: center; margin: -25px; font-size: 50px;">Make a donation!</h1>
+        <h1 style="text-align: center; margin: -25px; font-size: 30px;">Donate to transform lives in rural villages!</h1>
     </div>
 """
 
-# Dummy data representing poor villages
 descr1 = """Hope Haven - Kenya 
 Nestled in the rural outskirts of Nairobi, Kenya, Hope Haven is home to 500 resilient individuals. 
 However, with an 80% poverty rate, this village faces challenges in accessing clean water and educational 
 resources. Your support can bring hope and positive change to the lives of its residents."
 """
-descr2 = """Tranquil Meadows - India \n
+descr2 = """Tranquil Meadows - India
 Located in the remote mountains of Himachal Pradesh, India, Tranquil Meadows is a picturesque 
 village with 300 inhabitants. Despite its breathtaking landscapes, the village grapples with a 75% poverty rate 
 and lacks essential healthcare facilities. Your contribution can make a lasting impact on the well-being of this 
@@ -31,42 +27,41 @@ economic hardships and inadequate infrastructure. Your support can empower the r
 contribute to a brighter future."
 """
 
-# List of image paths or URLs
+# Consts
 images = ["vil1.jpg", "vil2.jpg", "vil3.jpg"]
 descriptions = [descr1, descr2, descr3]
-width_const = 800
-col1, col2, col3 = st.columns(3)
-custom_css = """
-        <style>
-            /* Example: Change the font to 'Arial, sans-serif' */
-            body {
-                font-family: 'Arial', sans-serif;
-            }
-        </style>
-    """
+width_const = 200
 
-def poor_village_image():
+# Create a session state to persist the current_index
+current_index = st.session_state.get("current_index", 0)
 
-    # Create a session state to persist the current_index
-    current_index = st.session_state.get("current_index", 0)
-
-    with col1:
-        if st.button("◀️ Previous", use_container_width=True):
-            current_index = (current_index - 1) % len(images)
-
-    with col2:
-        st.write("")
-
-    with col3:
-        if st.button("Next ▶️", use_container_width=True):
-            current_index = (current_index + 1) % len(images)
-
-    st.session_state.current_index = current_index
-
-    st.image(images[current_index], use_column_width=True)
-    st.text(descriptions[current_index])
-
-st.markdown(centered_title_html, unsafe_allow_html=True)
 st.markdown(centered_subtitle_html, unsafe_allow_html=True)
 
-poor_village_image()
+col0, col1, col2, col3, col4 = st.columns(5)
+
+with col0:
+    if st.button("◀️ Previous", use_container_width=True):
+        current_index = (current_index - 1) % len(images)
+
+with col4:
+    if st.button("Next ▶️", use_container_width=True):
+        current_index = (current_index + 1) % len(images)
+
+st.session_state.current_index = current_index
+
+st.image(images[current_index], width=width_const, use_column_width=True)
+
+centered_subtitle_html = f"""
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <p style="text-align: center; font-size: 14px;">{descriptions[current_index]}</p>
+    </div>
+"""
+
+st.markdown(centered_subtitle_html, unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([0.2, 0.6, 0.2])
+
+with col2:
+    donate = st.button("Make a donation!", use_container_width=True)
+    if donate:
+        print("Hello!")
